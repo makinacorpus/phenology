319c04058408c1ed207a3806d90ac3ee5566ba01
Julien Marinescu <jma@makina-corpus.com>
2014-07-23 13:35:14 +0200

    upload

--- www/js/home.js
+++ www/js/home.js
@@ -1,13 +1,32 @@
 'use strict';
 
-angular.module('home.controllers', ['synchronize'])
+angular.module('home.controllers', ['synchronize','ngAuthApiClient'])
 
-.controller('HomeCtrl', function($scope, synchronizeService) {
+.controller('HomeCtrl', function($scope, synchronizeService, authApiClient) {
     $scope.upcomming_tasks = [
         { title: 'Watch bud stage next month'},
         { title: 'Watch tree growth before summertime'}
     ];
     $scope.load = function() {
-        synchronizeService.loadUserSettings('user1');
+    	var username = authApiClient.getUsername();
+    	if (username == ""){
+    		$scope.login().then(function(){
+    			 $scope.load();
+    		})
+    	}
+    	else{
+    		 synchronizeService.loadUserSettings(username);
+    	}
+    };
+    $scope.upload = function(){
+        var username = authApiClient.getUsername();
+        if (username == ""){
+            $scope.login().then(function(){
+                 $scope.load();
+            })
+        }
+        else{
+             synchronizeService.uploadLocalSurveys();
+        }
     };
 });
