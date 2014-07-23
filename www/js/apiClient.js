e6a65969a5ac01e42778df4928e4aef0ae6608fb
Julien Marinescu <jma@makina-corpus.com>
2014-07-23 13:33:50 +0200

    api client

---
+++ www/js/apiClient.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('ngApiClient', ['ngResource', 'base64', 'ngStorageTraverser'])
+    .factory('apiClient', ["$resource", "$base64", "$http", "storageTraverser", function ($resource, $base64, $http, storageTraverser) {
+
+        // TODO : create a confValues
+        var backend_url = "http://127.0.0.1:8000"
+
+        // TODO : PUT/POST
+        
+        var resource =  $resource(backend_url + '/api/:action ',
+          { action:'@action', id:'@action_id'}, 
+          { 
+            get_user_settings: { url: backend_url + '/user_settings/  ', method:'GET'},
+            get_user_surveys: { url: backend_url + '/user_surveys/  ', method:'GET'},
+            create_survey: { url: backend_url + '/user_surveys/ ', method:'POST'},
+            save_survey: { url: backend_url + '/user_surveys/:id/ ', method:'PUT'}
+          }
+        );
+
+        return resource;
+}]);
