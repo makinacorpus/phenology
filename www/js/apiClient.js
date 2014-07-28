18ea99342954c39c7c20ae430d97edbf65d07026
Julien Marinescu <davisp@xenbox.fr>
2014-07-28 09:13:24 +0200

    fix error in apiClient

--- www/js/apiClient.js
+++ www/js/apiClient.js
@@ -12,7 +12,7 @@ angular.module('ngApiClient', ['ngResource', 'base64', 'ngStorageTraverser'])
           { action:'@action', id:'@action_id'}, 
           { 
             get_user_settings: { url: backend_url + '/user_settings/  ', method:'GET'},
-            get_user_surveys: { url: backend_url + '/user_surveys/  ', method:'GET'},
+            get_user_surveys: { url: backend_url + '/user_surveys/  ', isArray: true, method:'GET'},
             create_survey: { url: backend_url + '/user_surveys/ ', method:'POST'},
             save_survey: { url: backend_url + '/user_surveys/:id/ ', method:'PUT'}
           }
