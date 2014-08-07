7fe2d9923c70484ba2ff35f2664f23111a73c7e2
Julien Marinescu <davisp@xenbox.fr>
2014-08-07 12:04:34 +0200

    comment a bug

--- www/js/app.js
+++ www/js/app.js
@@ -78,9 +78,11 @@ angular.module('phenology', [
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    //broken
+    /**
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+    }**/
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
