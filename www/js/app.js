8f600d0bfc41cee42f6cf6278796a4bc55810a62
Julien Marinescu <davisp@xenbox.fr>
2014-09-02 13:44:01 +0200

    useful directive to set alternative src attribute if first one failed ( as 404 )

--- www/js/app.js
+++ www/js/app.js
@@ -213,6 +213,24 @@ angular.module('phenology', [
   $urlRouterProvider.otherwise('/app/home');
 
 
+})
+.directive('errSrc', function() {
+  return {
+    link: function(scope, element, attrs) {
+
+      scope.$watch(function() {
+          return attrs['ngSrc'];
+        }, function (value) {
+          if (!value) {
+            element.attr('src', attrs.errSrc);  
+          }
+      });
+
+      element.bind('error', function() {
+        element.attr('src', attrs.errSrc);
+      });
+    }
+  }
 });
 
 // UTILS
