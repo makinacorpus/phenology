7dcc23b26b4dab8f777ac6f95455bb72c7f08e91
Julien Marinescu <davisp@xenbox.fr>
2014-09-05 21:48:00 +0200

    changes on globalization service

--- www/js/globalization.js
+++ www/js/globalization.js
@@ -120,7 +120,7 @@ angular.module('phenology.globalization', ['ngCordova', 'tmh.dynamicLocale'])
         'nav.subheader.list': 'Liste',
         'nav.subheader.map': 'Carte',
         // button labels
-        'action.submit': 'Validez',
+        'action.submit': 'Valider',
         'action.submited': 'Validé',
         'action.synchronize': 'Synchroniser mes données',
         'action.close': 'Fermer',
@@ -153,7 +153,8 @@ angular.module('phenology.globalization', ['ngCordova', 'tmh.dynamicLocale'])
         'areas': 'Zones',
         'area': 'Zone',
         'survey': 'Observation',
-        'survey.change_stage': 'Changer le stade de développement',
+        'survey.change_stage': 'Choisir un autre stade',
+        'survey.date.title': 'Choisir une date',
         'snowcover': 'Enneigement',
         'to': 'au',
         'Flowering': 'Floraison',
