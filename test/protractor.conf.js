50ec1b0aa959038dd901e5581b9ae3a53150a60f
Julien Marinescu <davisp@xenbox.fr>
2014-09-08 08:22:43 +0200

    httpBackendMock + disable NgAnimate in e2e

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -23,6 +23,26 @@ exports.config = {
     isVerbose: true
   },
 
+  onPrepare: function() {
+
+    // Set the browser size with this
+    // browser.driver.manage().window().setSize(1200, 800);
+
+    // Disable Animations with this
+    var disableNgAnimate = function() {
+      angular.module('disableNgAnimate', []).run(function($animate) {
+        $animate.enabled(false);
+      });
+    };
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+
+    // Mock backend
+    var mocks = require('./utils/httpBackendMock.js');
+    if(process.env.BACKEND !== "real"){
+      browser.addMockModule('httpBackendMock', mocks.httpBackendMock);
+    }
+  },
+
   // The file path to the selenium server jar ()
   // tip to load automatically the selenium server
   //seleniumServerJar: path.resolve(projectRoot, 'node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar'),
