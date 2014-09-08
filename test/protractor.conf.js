fb87e89d2513d6b442110f7f34acad646160d696
Julien Marinescu <davisp@xenbox.fr>
2014-09-08 08:26:13 +0200

    use saucelab server to launch tests

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,14 +2,21 @@ var path = require('canonical-path');
 var projectRoot = path.resolve(__dirname, '..');
 
 exports.config = {
-  seleniumAddress: "http://localhost:4444/wd/hub",
+  //seleniumAddress: "http://localhost:4444/wd/hub",
   baseUrl: 'http://localhost:8100',
+
   capabilities: {
     'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
     'chromeOptions': {
       args: ['lang=en'] // don't work
     }   
   },
+
+  sauceUser: 'davisp1',
+  sauceKey: 'a151e8a4-3829-4875-b8b0-c6fcd35f3b5e',
+
   // Spec patterns are relative to the location of the spec file. They may
   // include glob patterns.
   specs: [
